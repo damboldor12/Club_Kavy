4dfe0f0a587e777e6c656736efe656e4be080108
damboldor12 <maksimkashubatop@gmail.com>
2023-08-31 18:07:41 +0300

    Перен

--- index.php
+++ index.php
@@ -1,11 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <link rel="stylesheet" href="style.css">
+    <script src="/script.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Club Kavy</title>
 </head>
+
 <body>
-    
+    <p id="main_description" class="main_description">Клуб Кави - це компанія з більш, як десятирічним досвідом в кавовій сфері України. Ми створюємо унікальні проекти, що ефективно працюють у різних напрямках: <br><br>
+        · Кав'ярні, які впізнають та люблять<br>
+        · Кава власного обсмаження<br>
+        · Послуги b2b для кавового бізнесу<br>
+        · Курси та навчання бариста<br>
+        · Різноманітні івенти та розважальні заходи</p>
+
+
+    <nav class="main_nav">
+        <a href="" id="nav_link_about" class="nav_link nav_link_active">Про нас</a>
+        <a href="" id="nav_link_projects" class="nav_link">Проєкти</a>
+        <a href="" id="nav_link_contacts" class="nav_link">Контакти</a>
+    </nav>
+
+    <div id="contacts" class="contacts">КОНТАКТИ</div>
+
 </body>
+
 </html>
---
+++ script.js
@@ -0,0 +1,226 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const NavLinks = this.getElementsByClassName("nav_link"); // Визначаємо рядки МЕНЮ
+    const navLinksArray = Array.from(NavLinks); // Створюємо МАСИВ рядків МЕНЮ
+
+
+    const mainDescription = document.getElementById('main_description'); // Визначаємо головний БЛОК ОПИСУ КОМПАНІЇ
+
+    const contacts = document.getElementById('contacts'); // Визначаємо блок з КОНТАКТАМИ
+    const projectContainer = document.body; // Визначаємо куди будемо вставляти блоки з ПРОЕКТАМИ
+
+
+    //ІНТЕРВАЛИ МІЖ АНІМАЦІЯМИ
+    let lastActionTime = 0; // Прописуємо початкове значення для інтервалу між анімаціями
+    const actionInterval = 300; // час між командами в мілісекундах
+
+    // МАСИВ З ІНФОРМАЦІЄЮ ПРО ПРОЕКТИ
+    const projects = [{
+        Name: 'Клуб Кави',
+        Logo: 'logo.png',
+        MiniLogo: '../img/ClubKavy.svg',
+        TextColor: 'black',
+        BackgroundColor: '#FFFFFF'
+    }, {
+        Name: 'MYATA',
+        Logo: 'logo.png',
+        MiniLogo: '../img/MYATA.svg',
+        TextColor: 'black',
+        BackgroundColor: '#C4DDDA'
+    }, {
+        Name: '4943',
+        Logo: 'logo.png',
+        MiniLogo: '../img/4943.svg',
+        TextColor: 'white',
+        BackgroundColor: 'black'
+    }, {
+        Name: 'Simple',
+        Logo: 'logo.png',
+        MiniLogo: '../img/SIMPLE.svg',
+        TextColor: 'white',
+        BackgroundColor: '#E84133'
+    }, {
+        Name: 'ROOF',
+        Logo: 'logo.png',
+        MiniLogo: '../img/ROOF.svg',
+        TextColor: 'black',
+        BackgroundColor: '#FFCC00'
+    }
+    ];
+    let translateX = 0; // Визначаємо початкову змінну для зсуву ТИТУЛКИ ПРОЕКТІВ
+    for (const projectKey in projects) { // Проходимо по кожному масиву ПРОЕКТУ
+        const project = projects[projectKey];
+
+        const projectDiv = document.createElement('div'); // створюємо DIV для кожного проекту
+        projectDiv.classList.add('ProjectElement'); // додаємо йму класс для стилів
+        projectDiv.style.color = `${project.TextColor}`; // визначаємо колір тексту з змінних
+        projectDiv.style.background = `${project.BackgroundColor}`;// визначаємо колір фону з змінних
+        projectDiv.innerHTML = `
+
+        <div class="Project_Title" style="transform: translate(${translateX}px, -95%); background:${project.BackgroundColor}">
+        <div class="Project_Title_Logo_Box"> <img class="Project_Title_Logo" src="${project.MiniLogo}"> </div>
+        ${project.Name}
+        </div>
+
+          <h2>${project.Name}</h2>
+        `;
+
+        projectContainer.appendChild(projectDiv); // вставляємо DIV в зарання визначений блок
+        translateX += 40; // робимо зсув титулки на кожному циклі
+    }
+
+    const ProjectElement = document.getElementsByClassName('ProjectElement'); // Визначаємо всі створені БЛОКИ З ПРОЕКТАМИ
+    const ProjectElementArray = Array.from(ProjectElement); // Створюємо МАСИВ з БЛОКІВ ПРОЕКТІВ
+    const numberOfProjects = Object.keys(projects).length;// Рахуємо КІЛЬКІСТЬ ПРОЕКТІВ
+
+
+    let ActiveProjectElement = 0; // Визначаємо який елемент буде активний з самого початку
+
+
+
+
+
+    function performAction() { // Функція, коли хочемо йти ВНИЗ по сайту
+        const currentTime = Date.now(); // записали поточний час
+
+
+        if (currentTime - lastActionTime >= actionInterval) { // перевіряємо чи пройшло достатньо часу
+            lastActionTime = currentTime; // записуємо коли спрацювала функція
+
+            if (ActiveProjectElement == numberOfProjects) { // Дивимось вже розгонтуто ОСТАНІЙ ЕЛЕМЕНТ з проектів
+                // якщо так - то хочваємо все і йдемо до блоку з КОНТАКТАМИ
+
+                // ХОВАЄМО ОПИС КОМПАНІЇ (за верхню частину сайту)
+                mainDescription.style.top = '-50% ';
+
+                //Беремо ВСІ ПРОЕКТИ І ховаємо в верх
+                ProjectElementArray.forEach(element => { element.style.transform = 'translate(0, -100vh)' });
+
+                //Виводимо блок контактів на пів екрану в верх
+                contacts.style.transform = 'translate(0, -50vh)';
+
+                navLinksArray.forEach(link => { // ЗНІМАЄМО клас АКТИВНОГО РЯДКА МЕНЮ з всіх рядків
+                    link.classList.remove('nav_link_active');
+                });
+
+                navLinksArray[2].classList.add('nav_link_active'); // Визначаємо ТРЕТІЙ ПУНКТ МЕНЮ АКТИВНИМ
+
+                ActiveProjectElement += 1; // Кажемо, що номер активного елементу БІЛЬШЕ, НІЖ ПРОЕКТІВ 
+                exit; // вихід
+            }
+
+            if (ActiveProjectElement == numberOfProjects + 1) { // Якщо ВЖЕ зайшли на ОДИН КРОК за ПРОЕКТИ
+                GoToSTART(); // повертаємо на головний екран все
+            }
+
+            if (ActiveProjectElement < numberOfProjects) {
+                console.log('< numberOfProjects');
+                // Ваш код для виконання дії при свайпі вгору або вниз
+                mainDescription.style.top = '10% ';
+                navLinksArray.forEach(link => {
+                    link.classList.remove('nav_link_active');
+                });
+                navLinksArray[1].classList.add('nav_link_active');
+
+                ProjectElement[ActiveProjectElement].style.transform = 'translate(0, 0)';
+                ActiveProjectElement += 1;
+            }
+
+
+        }
+    }
+
+    function performAction_second() {
+        const currentTime = Date.now();
+
+        if (currentTime - lastActionTime >= actionInterval) {
+            lastActionTime = currentTime;
+
+
+
+            if (ActiveProjectElement > 0 && ActiveProjectElement <= numberOfProjects) {
+                ActiveProjectElement -= 1;
+                // Ваш код для виконання дії при свайпі вгору або вниз
+                if (ActiveProjectElement == 0 || ActiveProjectElement == numberOfProjects + 1) {
+                    mainDescription.style.top = '45%';
+                    navLinksArray.forEach(link => {
+                        link.classList.remove('nav_link_active');
+                    });
+                    navLinksArray[0].classList.add('nav_link_active');
+                }
+                ProjectElement[ActiveProjectElement].style.transform = 'translate(0, 100%)';
+            }
+
+            if (ActiveProjectElement > numberOfProjects) {
+                mainDescription.style.top = '10% ';
+                ProjectElementArray.forEach(element => { element.style.transform = 'translate(0, 0)' });
+                contacts.style.transform = 'translate(0, 100%)';
+                navLinksArray.forEach(link => {
+                    link.classList.remove('nav_link_active');
+                });
+                navLinksArray[1].classList.add('nav_link_active');
+                ActiveProjectElement -= 1;
+            }
+        }
+    }
+
+
+
+    let touchStartY = 0;
+    function handleTouchStart(event) {
+        touchStartY = event.touches[0].clientY;
+    }
+
+    function handleTouchEnd(event) {
+        const touchEndY = event.changedTouches[0].clientY;
+        const deltaY = touchEndY - touchStartY;
+
+        if (deltaY > 0) {
+            performAction_second();
+            // Користувач зробив свайп вниз
+        } else if (deltaY < 0) {
+            // Користувач зробив свайп вверх
+            performAction();
+        }
+    }
+
+    document.addEventListener('touchstart', handleTouchStart);
+    document.addEventListener('touchend', handleTouchEnd);
+
+
+
+    function handleWheel(event) {
+        event = event || window.event; // Для кросбраузерності
+        const deltaY = event.deltaY || event.detail || (-event.wheelDelta); // Для різних браузерів
+        if (deltaY > 0) {
+            // Користувач прокрутив вниз
+            performAction();
+        } else if (deltaY < 0) {
+            performAction_second();
+        }
+    }
+
+    if (document.addEventListener) {
+        // Сучасні браузери, які підтримують addEventListener
+        document.addEventListener('wheel', handleWheel);
+        document.addEventListener('DOMMouseScroll', handleWheel); // Firefox
+    } else if (document.attachEvent) {
+        // Старі версії IE
+        document.attachEvent('onmousewheel', handleWheel);
+    }
+});
+
+function GoToSTART() {
+    mainDescription.style.top = '45%';
+
+    navLinksArray.forEach(link => {
+        link.classList.remove('nav_link_active');
+    });
+    navLinksArray[0].classList.add('nav_link_active');
+
+    contacts.style.transform = 'translate(0, 100%)';
+
+    ProjectElementArray.forEach(element => { element.style.transform = 'translate(0, 100%)' });
+    ActiveProjectElement = 0;
+
+    exit;
+}
